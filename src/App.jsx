2887d5039822234cfacc30a13426e9861ab5b16f
Luuk05 <74959722+Luuk05@users.noreply.github.com>
2022-05-01 17:09:10 +0200

    Oefenen

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import "./App.css"
-import { Suspense, useEffect, useState } from "react";
+import { Suspense, useEffect, useState, useRef} from "react";
 import styled from 'styled-components';
 import * as THREE from 'three'
-import { Canvas, useFrame } from '@react-three/fiber';
+import { Canvas, useFrame, useThree } from '@react-three/fiber';
 import { OrbitControls, Lightformer, GradientTexture, Environment, Float, ContactShadows, PerspectiveCamera } from "@react-three/drei";
 import { useControls } from 'leva'
 import { LayerMaterial, Base, Depth, Noise, Gradient, Color } from 'lamina'
@@ -13,11 +13,26 @@ import Car from './components/Car';
 import Loader from './components/Loader';
 
 let App = () => {
+    // function Camera(props) {
+  //   const ref = useRef()
+  //   console.log(ref)
+  //   // ref.updateProjectionMatrix()
+  //   return <perspectiveCamera ref={ref} {...props} />
+  // }
 
+  function MoveCamera(props) {
 
+    useFrame((state) => {
+      state.camera.zoom += 0.001
+      
+    })
+    // return null
+  }
+  
   return (
     <Canvas>
-      <PerspectiveCamera makeDefault position={[0, 1.2, 12]} fov={30} zoom={1} />
+      <PerspectiveCamera makeDefault position={[0, 1.2, 12]} fov={30} zoom={0.1} />
+      {/* <Camera makeDefault position={[0, 1.2, 12]} fov={30} zoom={0.1} /> */}
       <Suspense fallback={null}>
         <spotLight position={[0, 15, 0]} angle={0.3} penumbra={1} castShadow intensity={2} shadow-bias={-0.0001} />
         <ambientLight intensity={0.2} />
@@ -43,6 +58,7 @@ let App = () => {
         </LayerMaterial>
       </mesh>
       
+      <MoveCamera />
     </Canvas>
   );
 }
